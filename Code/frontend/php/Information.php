9d3138af4f0901769903c16fbe40567a5be6e123
kranslam <kransalm@gmail.com>
2018-03-29 14:06:09 -0700

    pdf implementation complete

--- Code/frontend/php/Information.php
+++ Code/frontend/php/Information.php
@@ -1,66 +1,223 @@
-<!DOCTYPE html>
-<html>
-
-<p>Here are some results:</p>
-
 <?php
+require('../../backend/fpdf.php');
 
-$host = "localhost";
-$database = "mfac";
-$user = "mfac";
-$password = "v9KdEMGL";
+$host     = "localhost";
+$database = "mfc";
+$user     = "root";
+$password = "";
 
 $connection = mysqli_connect($host, $user, $password, $database);
 
 $error = mysqli_connect_error();
-if($error != null)
-{
-  $output = "<p>Unable to connect to database!</p>";
-  exit($output);
-}
-else
-{
+if ($error != null) {
+    //  $output = "<p>Unable to connect to database!</p>";
+    exit();
+} else {
     //variable list
-    $email=$_POST['email'];
-    $bday=$_POST['bday'];
-    $gender=(isset($_POST['gender']) ? $_POST['gender'] : null);
-    $tel=$_POST['tel'];
-    $BuildingType=(isset($_POST['BuildingType']) ? $_POST['BuildingType'] : null);
-    $CaseManagementServices=(isset($_POST['CaseManagmentServices']) ? $_POST['CaseManagmentServices'] : null);
-    $ReferralsToOtherAgencies=(isset($_POST['ReferralsToOtherAgencies']) ? $_POST['ReferralsToOtherAgencies'] : null);
-    $HealthDentalCare=(isset($_POST['Health/DentalCare']) ? $_POST['Health/DentalCare'] : null);
-    $meals=(isset($_POST['meals']) ? $_POST['meals'] : null);
-    $Showers=(isset($_POST['Showers']) ? $_POST['Showers'] : null);
-    $Storage=(isset($_POST['Storage']) ? $_POST['Storage'] : null);
-    $ClothingHouseholdGoods=(isset($_POST['Clothing/HouseholdGoods']) ? $_POST['Clothing/HouseholdGoods'] : null);
-    $OtherServices=(isset($_POST['Text1']) ? $_POST['Text1'] : null);
-    $NumberOfBeds=(isset($_POST['NumberOfBeds']) ? $_POST['NumberOfBeds'] : null);
-    $OtherBedroomUnits=(isset($_POST['OtherBedroomUnits']) ? $_POST['OtherBedroomUnits'] : null);
-    $disabilities=(isset($_POST['disabilities']) ? $_POST['disabilities'] : null);
-    $aboriginal=(isset($_POST['aboriginal']) ? $_POST['aboriginal'] : null);
-    $budget=(isset($_POST['budget']) ? $_POST['budget'] : null);
-    $FixedIncome=(isset($_POST['FixedIncome']) ? $_POST['FixedIncome'] : null);
-    $DisabilityAssistance=(isset($_POST['DisabilityAssistance']) ? $_POST['DisabilityAssistance'] : null);
-    $PetFriendly=(isset($_POST['PetFriendly']) ? $_POST['PetFriendly'] : null);
-    $drinker=(isset($_POST['drinker']) ? $_POST['drinker'] : null);
-    $smoker=(isset($_POST['smoker']) ? $_POST['smoker'] : null);
-    $ProfessionalSupport=(isset($_POST['ProfessionalSupport']) ? $_POST['ProfessionalSupport'] : null);
+    $gender                              = (isset($_POST['gender']) ? $_POST['gender'] : null);
+    $age                                 = (isset($_POST['age']) ? $_POST['age'] : null);
+    $providesServicesToDemographic16to18 = 0;
+    $providesServiceToDemographic19      = 0;
+    $providesServiceToDemographic55      = 0;
+    $providesServiceToOtherDemographic   = 0;
+    if ($age == 'under19') {
+        $providesServicesToDemographic16to18 = 1;
+    } elseif ($age == 'over19') {
+        $providesServiceToDemographic19 = 1;
+    } elseif ($age == 'Senior') {
+        $providesServiceToDemographic55 = 1;
+    } else {
+        $providesServiceToOtherDemographic = 1;
+    }
+    $suitablefor = (isset($_POST['group']) ? $_POST['group'] : null);
+    $individual  = 0;
+    $couples     = 0;
+    $family      = 0;
+    $othergroup  = 0;
+    if ($suitablefor == 'individual') {
+        $individual = 1;
+    } elseif ($suitablefor == 'couples') {
+        $couples = 1;
+    } elseif ($suitablefor == 'family') {
+        $family = 1;
+    } else {
+        $othergroup = 1;
+    }
+    $availableNow       = (isset($_POST['availableNow']) ? 0 : 1);
+    $BuildingType       = (isset($_POST['BuildingType']) ? $_POST['BuildingType'] : null);
+    $buildingdisability = (isset($_POST['buildingdisability']) ? 1 : 0);
+    $unitdisability     = (isset($_POST['unitdisability']) ? 1 : 0);
+    $rgi                = (isset($_POST['rgi']) ? 1 : 0);
+    $cost               = (isset($_POST['cost']) ? $_POST['cost'] : null);
+    $PetFriendly        = (isset($_POST['PetFriendly']) ? 1 : 0);
+
     //query
-    echo $HealthDentalCare;
-    echo $smoker;
-    $sql = "SELECT * FROM mfac;";
+
+    // $sql = "SELECT  DISTINCT title, coordinate, description,
+    // buildingType, buildingQualifiesForRentalAssistance, monthlyCostOfStay,
+    // hasWaitingList, estimatedWaitTime, hasMaximumDurationOfStay,
+    // maximumDurationOfStay, servicesProvided, otherServices, buildingIsPetFriendly,
+    // petRestriction FROM mfc WHERE unitsAreRGI=$rgi OR genderServed LIKE '%$gender%'
+    // OR buuildingIsPetFriendly=$PetFriendly OR
+    // providesServicesToDemographic16to18=$providesServicesToDemographic16to18 OR
+    // providesServiceToDemographic19=$providesServiceToDemographic19 OR
+    // providesServiceToDemographic55=$providesServiceToDemographic55 OR
+    // providesServiceToOtherDemographic=$providesServiceToOtherDemographic OR
+    // primaryTargetResidentsAreIndividuals=$individual OR primaryTargetResidentsAreFamilies=$family OR monthlyCostOfStay LIKE '%$cost%'
+    // OR buildingType LIKE '%$BuildingType%' OR hasWaitingList=$availableNow ;";
+    $sql     = "SELECT  DISTINCT * FROM mfc WHERE unitsAreRGI=$rgi OR genderServed LIKE '%$gender%'
+OR buuildingIsPetFriendly=$PetFriendly OR
+providesServicesToDemographic16to18=$providesServicesToDemographic16to18 OR
+providesServiceToDemographic19=$providesServiceToDemographic19 OR
+providesServiceToDemographic55=$providesServiceToDemographic55 OR
+providesServiceToOtherDemographic=$providesServiceToOtherDemographic OR
+primaryTargetResidentsAreIndividuals=$individual OR primaryTargetResidentsAreFamilies=$family OR monthlyCostOfStay LIKE '%$cost%'
+OR buildingType LIKE '%$BuildingType%' OR hasWaitingList=$availableNow ;";
+    // $sql="SELECT * FROM mfc;";
     $results = mysqli_query($connection, $sql);
-    while ($row = mysqli_fetch_assoc($results))
-    {
-      /*
-      foreach($row as $field) {
-        echo '<p>' . htmlspecialchars($field) . '</p>';
+
+    $pdf = new FPDF();
+    $pdf->AddPage();
+    while ($row = mysqli_fetch_assoc($results)) {
+        $pdf->SetFont('Arial', 'b', 15);
+        $pdf->MultiCell(180, 10, $row['title']);
+        $pdf->SetFont('Arial', 'u', 10);
+
+        $pdf->MultiCell(50, 10, 'Location: ' );
+        $pdf->SetFont('');
+        $pdf->MultiCell(180, 10, $row['coordinate']);
+
+        $pdf->SetFont('Arial', 'u', 10);
+        $pdf->MultiCell(50, 10, 'Description: ' );
+        $pdf->SetFont('');
+        $pdf->MultiCell(180, 10, isZero($row['description']));
+
+        $pdf->SetFont('Arial', 'u', 10);
+        $pdf->MultiCell(50, 10, 'Building Type: ');
+        $pdf->SetFont('');
+        $pdf->MultiCell(180, 10, $row['buildingType']);
+
+        $pdf->SetFont('Arial', 'b', 11);
+        $pdf->MultiCell(180, 10, "Cost");
+
+        $pdf->SetFont('Arial', 'u', 10);
+        $pdf->MultiCell(90, 10, 'Eligible for income/disability assistance?: ');
+        $pdf->SetFont('');
+        $pdf->MultiCell(180, 10, isZero($row['buildingQualifiesForRentalAssistance']));
+
+        $pdf->SetFont('Arial', 'u', 10);
+        $pdf->MultiCell(50, 10, 'Rent/costs? ');
+        $pdf->SetFont('');
+        $pdf->MultiCell(180, 10, $row['monthlyCostOfStay']);
+
+        $pdf->SetFont('Arial', 'u', 10);
+        $pdf->MultiCell(50, 10, 'Is there a waitlist? ');
+        $pdf->SetFont('');
+        $pdf->MultiCell(180, 10,  isZero($row['hasWaitingList']));
+
+        $pdf->SetFont('Arial', 'u', 10);
+        $pdf->MultiCell(50, 10, 'Estimated Wait Time: ');
+        $pdf->SetFont('');
+        $pdf->MultiCell(180, 10,$row['estimatedWaitTime']);
+
+        $pdf->SetFont('Arial', 'u', 10);
+        $pdf->MultiCell(80, 10, 'Has maximum duration of stay? ');
+        $pdf->SetFont('');
+        $pdf->MultiCell(180, 10, isZero($row['hasMaximumDurationOfStay']));
+
+        $pdf->SetFont('Arial', 'u', 10);
+        $pdf->MultiCell(50, 10, 'Services provided: ');
+        $pdf->SetFont('');
+        $pdf->MultiCell(180, 10, $row['servicesProvided']);
+
+        $pdf->SetFont('Arial', 'u', 10);
+        $pdf->MultiCell(50, 10, 'Other Services Provided: ');
+        $pdf->SetFont('');
+        $pdf->MultiCell(180, 10, isZero($row['otherServices']));
+
+        $pdf->SetFont('Arial', 'b', 11);
+        $pdf->MultiCell(180, 10, "Restrictions");
+
+        $pdf->SetFont('Arial', 'u', 10);
+        $pdf->MultiCell(50, 10, 'Pet Friendly? ' );
+        $pdf->SetFont('');
+        $pdf->MultiCell(180, 10, isZero($row['buuildingIsPetFriendly']));
+
+        $pdf->SetFont('Arial', 'u', 10);
+        $pdf->MultiCell(50, 10, 'Pet Restrictions: ');
+        $pdf->SetFont('');
+        $pdf->MultiCell(180, 10, $row['petRestriction']);
+
+        $pdf->SetFont('Arial', 'u', 10);
+        $pdf->MultiCell(50, 10, 'Smoking? ');
+        $pdf->SetFont('');
+        $pdf->MultiCell(180, 10, isZero($row['smokingIsPermittedInBuilding']));
+
+        $pdf->SetFont('Arial', 'u', 10);
+        $pdf->MultiCell(50, 10, 'Drug / Alcohol Tolerance: ');
+        $pdf->SetFont('');
+        $pdf->MultiCell(180, 10, isZero($row['levelOfDrugsAndAlcoholTolerence']));
+
+        $pdf->SetFont('Arial', 'u', 10);
+        $pdf->MultiCell(50, 10, 'Demographics Accomodated: ');
+        $pdf->SetFont('');
+        $pdf->MultiCell(180, 10,  $row['demographicServed']);
+
+        $pdf->SetFont('Arial', 'u', 10);
+        $pdf->MultiCell(50, 10, 'Genders Accomodated: ');
+        $pdf->SetFont('');
+        $pdf->MultiCell(180, 10, $row['genderServed']);
+
+        $pdf->SetFont('Arial', 'u', 10);
+        $pdf->MultiCell(50, 10, 'People Accommodated: ');
+        $pdf->SetFont('');
+        $pdf->MultiCell(180, 10, $row['primaryTarget']);
+
+        $pdf->AddPage();
+
+
+        // $pdf->MultiCell(40,10,'Column: '.$row['title']);
+        // foreach($row as $field) {
+        //
+        //   if ($count==120) {
+        //     // $pdf->AddPage();
+        //     $pdf->SetFont('Arial','b',15);
+        //     $pdf->MultiCell(40,10,"Organization Name");
+        //     $count=0;
+        //     $pdf->SetFont('Arial','',11);
+        //
+        //   }
+        //   // $pdf->Cell(10, 10, 'Column: ');
+        //   $pdf->MultiCell(180,10,'Column: '.$field);
+        //   $count++;
+        // }
+
     }
-    */
-    }
-    mysqli_free_result($results);
+    // mysqli_free_result($results);
     mysqli_close($connection);
 
 }
+$pdf->Output();
+
+function isZero($value)
+{
+    if (!is_array($value)) {
+        if (!is_string($value)) {
+            if ($value == 0) {
+                return 'No';
+            } else {
+                return 'Yes';
+            }
+        } else if (empty($value)) {
+            return 'N/A';
+        } else{
+            return $value;
+        }
+    }else //is an array
+    if (empty($value)) {
+        return 'N/A';
+    } else {
+        return $value;
+    }
+}
 ?>
-</html>
